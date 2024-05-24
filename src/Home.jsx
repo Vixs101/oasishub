b8dffc121c22897bd7e4f98447b92c24548118dd
Vixs101 <elijahvix695@gmail.com>
2024-05-24 17:13:14 +0100

    finished the responsive design for the membership section

--- src/Home.jsx
+++ src/Home.jsx
@@ -39,8 +39,15 @@ function Home() {
         p-5 my-3
         ">
         <div className="flex flex-col gap-2 w-full">
-          <h2 className="text-3xl md:text-5xl font-extrabold">Features</h2>
-          <p className="text-gray-500">
+          <h2 
+            className="
+              text-3xl md:text-5xl font-extrabold
+            ">
+              Features
+          </h2>
+          <p 
+            className="text-gray-500"
+          >
             Discover the amazing features of our Tech Hub
           </p>
         </div>
@@ -79,11 +86,16 @@ function Home() {
       </section>
 
       {/* images */}
-      <section className="appWidth pb-20 bg-[#023A7B] w-full h-screen flex flex-wrap gap-4 items-center justify-center">
+      <section 
+        className="
+          appWidth md:pb-20 bg-[#023A7B] w-full h-screen md:flex md:flex-wrap md:gap-4 md:items-center md:justify-center
+          grid grid-cols-2 gap-3 pb-32
+          "
+        >
         {pictures.map((pic, index) => (
           <div 
             key={index}
-            className="h-1/6 w-1/6"
+            className="md:h-1/6 md:w-1/6"
           >
             <img
               src={`/images/${pic.picture}.png`}
@@ -95,25 +107,55 @@ function Home() {
         ))}
       </section>
 
-      <section className="appWidth w-full h-screen bg-[#E7FDE2] ">
-        <div className="flex flex-col gap-2 w-5/13">
-          <h2 className="text-5xl font-extrabold">Membership Options</h2>
+      <section 
+        className="
+          appWidth w-full md:h-screen bg-[#E7FDE2] 
+        ">
+        <div 
+          className="
+            flex flex-col gap-2 md:w-5/13
+            w-full  
+            "
+          >
+          <h2 
+            className="
+              text-3xl md:text-5xl font-extrabold
+          ">
+            Membership Options
+          </h2>
           <p className="text-gray-500">
             Choose The membership option that suits your needs and join our
             vibrant tech hub or workspace
           </p>
         </div>
 
-        <div className="flex gap-2 py-10 h-5/7">
+        <div 
+          className="
+            flex md:flex-row md:gap-2 py-10 h-5/7
+            flex-col gap-4
+          ">
           {membership.map((members, index) => (
-            <div key={index} className="flex flex-col gap-2 bg-white px-4 pt-4 pb-12 rounded-2xl h-full">
+            <div 
+              key={index} 
+              className="
+                flex flex-col gap-2 bg-white px-4 pt-4 pb-12 rounded-2xl h-full
+                
+              ">
               <img
                 src={`/images/${members.member}.png`}
                 alt="A member of the hub"
                 className="h-2/3"
               />
-              <h2>{members.title}</h2>
-              <p>{members.content}</p>
+              <h2 
+                className="
+                  font-bold text-lg md:text-xl
+                "
+              >
+                {members.title}
+              </h2>
+              <p>
+                {members.content}
+              </p>
             </div>
           ))}
         </div>
